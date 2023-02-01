8f79ccdc8b1ec85bfb8622bdc2fef314d5ddd265
OdetteArriola <odette.arriola@gmail.com>
2023-02-01 16:24:30 -0600

    detail listo

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -4,22 +4,10 @@ import NavBar from "../../components/NavBar/NavBar";
 
 const Home = () => {
 
-    ///funciones handlers------------
-    // function handleOrderByName(e) {
-    //     e.preventDefault();
-    //     dispatch(orderByName(e.target.value));
-    //     setCurrentPage(1);
-    //     setOrden(e.target.value);
-    // }
-
-
-    ///--------------------------
-
     return(
         <div>
             <NavBar />
             <CardsContainer />
-
        </div>
     )
 }
