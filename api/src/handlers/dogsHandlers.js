ce3fbb742e502d0a1a9e54c7d80181f2f93ba9b6
OdetteArriola <odette.arriola@gmail.com>
2023-02-01 11:14:40 -0600

    Detalle de back corregido

--- api/src/handlers/dogsHandlers.js
+++ api/src/handlers/dogsHandlers.js
@@ -10,18 +10,73 @@ const getDogsHandler = async (req, res) => {
 
 };
 
-const getDogHandler = async (req, res) => {
-    const { id } = req.params;
 
-    const source = isNaN(id) ? "bdd" : "api";
+///ORIGINAL
+// const getDogHandler = async (req, res) => {
+//     const { id } = req.params;
 
-    try{
-        const dog = await getDogById(id, source);
-        res.status(200).json(dog);
-    } catch (error){
-        res.status(400).json({ error: error.message })
+//     const source = isNaN(id) ? "bdd" : "api";
+
+//     try{
+//         const dog = await getDogById(id, source);
+//         res.status(200).json(dog);
+//     } catch (error){
+//         res.status(400).json({ error: error.message })
+//     }
+// };
+
+
+///JORGE
+// const getDogHandler = async (req, res) => {
+//     /* http://localhost:3001/dogs/7 */
+//     try {
+//       const { id } = req.params;
+//       const allDogs = await getAllDogs();
+//       if (!id) {
+//         res.status(404).json("Couldn't find the name on DBase");
+//       } else {
+//         const dog = allDogs.find((dogui) => dogui.id.toString() === idRaza);
+//         res.status(200).json(dog);
+//       }
+//     } catch (error) {
+//       res.status(404).send(error);
+//     }
+//   };
+
+
+  ///AGUS
+  const getDogHandler = async (req, res) => {
+    const id = req.params.id;
+    try {
+      let allDogs = await getAllDogs();
+      if (id) {
+        let dogsById = await allDogs.filter((e) => e.id == id);
+        dogsById.length
+          ? res.status(200).json(dogsById)
+          : res.status(404).send("Doesn't exist");
+      } else {
+        res.status(400).send("Sorry, couldn't find the dog");
+        
+      }
+    } catch (error) {
+      res.status(400).send("Error");
     }
-};
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const createDogHandler = async (req, res) => {
     const {name, height_min, height_max, weight_min, weight_max, life_span, temperament, image } = req.body;
